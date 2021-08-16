d69036d34054d15b86ea1126a191f6aab5d3fdf7
Cannon <teamnapinfo@gmail.com>
2021-08-16 13:40:59 +0100

    init

---
+++ src/Client.ts
@@ -0,0 +1,8 @@
+import { Client as DJSClient } from "discord.js"
+import { ClientTypes } from "./types" // hello :D
+
+export default class Client extends DJSClient {
+    constructor() {
+        super()
+    }
+}
